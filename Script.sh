792494ed2124d4a1c5bfa34de6e35e1be6f00170
Élian <elianjacobe@hotmail.ca>
2019-12-09 13:57:02 -0500

    Ajout utilisateur
    Mot de passe
    dossier home

--- Script.sh
+++ Script.sh
@@ -1,4 +1,17 @@
+#! /bin/bash
+
 $ip = ifconfig -a
 echo "IP:"
 echo $ip
 
+USER_NAME=elian
+cat /etc/passwd | grep ${USER_NAME} >/dev/null 2>&1
+if [ $? -eq 0 ] ; then
+    echo "User Exists"
+else
+    echo "User Not Found"
+	echo "Creating user..."
+	useradd -m elian
+	echo -e "tge247\ntge247" | passwd elian
+	echo "User created!"
+fi
